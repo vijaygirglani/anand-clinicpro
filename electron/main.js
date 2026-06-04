e7eb83601040e2ea69884bc58a45ea6ce7889a69
ClinicPro <clinicpro@manglam.local>
2026-06-04 15:12:55 +0530

    chore: remove auto-open DevTools

--- electron/main.js
+++ electron/main.js
@@ -69,7 +69,6 @@ function createWindow() {
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
     mainWindow.maximize();
-    mainWindow.webContents.openDevTools();
   });
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
