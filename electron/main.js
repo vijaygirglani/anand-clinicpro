7a1c5c08d54efb3d0ef7b941dcee53a264af495a
ClinicPro <clinicpro@manglam.local>
2026-06-04 15:12:22 +0530

    debug: auto-open DevTools on app launch

--- electron/main.js
+++ electron/main.js
@@ -69,6 +69,7 @@ function createWindow() {
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
     mainWindow.maximize();
+    mainWindow.webContents.openDevTools();
   });
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
